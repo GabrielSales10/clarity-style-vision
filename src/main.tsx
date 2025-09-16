e86bd635b1a262b825eb42512b435da0609e47d4
Gabriel Sales <99056850+GabrielSales10@users.noreply.github.com>
2025-09-16 15:57:32 -0300

    Integrate Lenis for smooth scrolling in main.tsx
    Added Lenis for smooth scrolling and updated rendering method.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,28 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Lenis para smooth scrolling global
+import Lenis from "lenis";
+
+const lenis = new Lenis({
+  duration: 1.1,
+  smoothWheel: true,
+  smoothTouch: false,
+});
+
+function raf(time: number) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
